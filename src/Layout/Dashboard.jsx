4125755aacc0c5092f9a10d61117917c9c28d70e
hamed18 <hamedcnec@gmail.com>
2024-09-17 02:10:46 +0600

    added Dashboard component

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,12 +1,92 @@
-import { FaAd, FaBook, FaCalendar, FaEnvelope, FaHome, FaList, FaSearch, FaShoppingCart, FaUsers, FaUtensils } from "react-icons/fa";
-import { NavLink, Outlet } from "react-router-dom";
-
-const Dashboard = () => {
+import {
+	FaHome,
+	FaList,
+	FaUtensils,
+	FaBook,
+	FaUsers,
+	FaCalendar,
+	FaShoppingCart,
+	FaAd,
+  } from "react-icons/fa";
+  import { NavLink, Outlet } from "react-router-dom";
+  import useHR from "../hooks/useHR";
+  
+  const Dashboard = () => {
+	const [isHR] = useHR();
 	return (
-		<div>
-			<h3>Dashboard</h3>
+	  <div className="flex">
+		{/* dashboard side bar */}
+		<div className="w-64 min-h-screen bg-orange-400">
+		  <ul className="menu p-4">
+			{isHR ? (
+			  <>
+				{/* HR */}
+				<li>
+				  <NavLink to="/dashboard/HRhome">
+					<FaHome /> Home
+				  </NavLink>
+				</li>
+				<li>
+				  <NavLink to="/dashboard/AddAsset">
+					<FaUtensils /> Add Asset
+				  </NavLink>
+				</li>
+				<li>
+				  <NavLink to="/dashboard/AddEmployee">
+					<FaList /> Add Employee
+				  </NavLink>
+				</li>
+				<li>
+				  <NavLink to="/dashboard/assetList">
+					<FaBook /> Asset List
+				  </NavLink>
+				</li>
+				<li>
+				  <NavLink to="/dashboard/employeeList">
+					<FaUsers /> Employee List
+				  </NavLink>
+				</li>
+			  </>
+			) : (
+			  <>
+				<li>
+				  <NavLink to="/dashboard/employeeHome">
+					<FaHome /> Home
+				  </NavLink>
+				</li>
+				<li>
+				  <NavLink to="/dashboard/employeeAssets">
+					<FaCalendar /> My Assets
+				  </NavLink>
+				</li>
+				<li>
+				  <NavLink to="/dashboard/employeeTeam">
+					<FaShoppingCart /> My Team
+				  </NavLink>
+				</li>
+				<li>
+				  <NavLink to="/dashboard/employeeRequest">
+					<FaAd /> Request for Asset
+				  </NavLink>
+				</li>
+			  </>
+			)}
+			{/* shared nav links */}
+			<div className="divider"></div>
+			<li>
+			  <NavLink to="/">
+				<FaHome /> Home
+			  </NavLink>
+			</li>
+		  </ul>
 		</div>
+		{/* dashboard content */}
+		<div className="flex-1 p-8">
+		  <Outlet />
+		</div>
+	  </div>
 	);
-};
-
-export default Dashboard;
+  };
+  
+  export default Dashboard;
+  
